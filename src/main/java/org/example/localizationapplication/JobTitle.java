5cecc5746d8fcdd9a67a84928eb1b9842319921f
Lucas <lupelikan@int.pl>
2025-04-03 13:06:08 +0300

    added job title class

---
+++ src/main/java/org/example/localizationapplication/JobTitle.java
@@ -0,0 +1,17 @@
+package org.example.localizationapplication;
+
+public class JobTitle {
+    private String jobTitle;
+
+    public JobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+}
